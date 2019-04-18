0d5fcf506a91d5ecce74fe604e206723708afff0
chenb <215109103@qq.com>
2019-04-18 09:58:41 +0800

    基础类

---
+++ src/main/java/com/java/steps/leetCode/TreeNode.java
@@ -0,0 +1,13 @@
+package com.java.steps.leetCode;
+/**
+ * 树节点
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
